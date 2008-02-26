00e070b5dbad1dca15fe46c74582e7e2c1c61783
Rodrigo Franco (Caffo) <caffeine@gmail.com>
2008-02-26 17:03:21 -0300

    Added test sqlite file to .gitingore and done some work on books functional tests

--- test/functional/books_controller_test.rb
+++ test/functional/books_controller_test.rb
@@ -6,6 +6,7 @@ class BooksController; def rescue_action(e) raise e end; end
 
 class BooksControllerTest < Test::Unit::TestCase
   fixtures :books
+  fixtures :users
 
   def setup
     @controller = BooksController.new
@@ -13,48 +14,41 @@ class BooksControllerTest < Test::Unit::TestCase
     @response   = ActionController::TestResponse.new
   end
 
-  def test_should_require_login
-    assert_requires_login(:quentin) { |c| 
-      c.get :edit, :id => 1 
-      c.get :new
-    }
-  end
-
   def test_should_get_index
+    login_as :quentin    
     get :index
     assert_response :success
-    assert assigns(:books)
   end
 
   def test_should_get_new
+    login_as :quentin    
     get :new
     assert_response :success
   end
   
   def test_should_create_book
+    login_as :quentin
     old_count = Book.count
     post :create, :book => { }
     assert_equal old_count+1, Book.count
     
-    assert_redirected_to book_path(assigns(:book))
-  end
-
-  def test_should_show_book
-    get :show, :id => 1
-    assert_response :success
+    assert_redirected_to books_path
   end
 
   def test_should_get_edit
+    login_as :quentin    
     get :edit, :id => 1
     assert_response :success
   end
   
   def test_should_update_book
+    login_as :quentin    
     put :update, :id => 1, :book => { }
-    assert_redirected_to book_path(assigns(:book))
+    assert_redirected_to books_path
   end
   
   def test_should_destroy_book
+    login_as :quentin
     old_count = Book.count
     delete :destroy, :id => 1
     assert_equal old_count-1, Book.count
