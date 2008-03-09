2a1595d40d7d75d855d5202ccea089c2e4ff7871
Rodrigo Franco (Caffo) <caffeine@gmail.com>
2008-03-09 20:47:41 -0300

    cover.rb now uses rmagick as default processor

--- app/models/cover.rb
+++ app/models/cover.rb
@@ -1,6 +1,7 @@
 class Cover < ActiveRecord::Base
   has_attachment  :content_type => :image, 
-                  :storage => :file_system, 
-                  :max_size => 900.kilobytes
+                  :storage      => :file_system, 
+                  :max_size     => 900.kilobytes,
+                  :processor    => :Rmagick
   belongs_to :book
 end
