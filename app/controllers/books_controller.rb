281c80f0b0d2db6f02b5dde61e60bb8b6e920a12
Rodrigo Franco (Caffo) <caffeine@gmail.com>
2008-03-12 02:36:18 -0300

    Initial work on the book page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-before_filter :login_required, :except => [:index,:rss]
+before_filter :login_required, :except => [:index,:rss,:show]
 
   # GET /books
   # GET /books.xml
