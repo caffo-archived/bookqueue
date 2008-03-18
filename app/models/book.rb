b2104c3405fda30931c0404256e49afaec20d8ed
Rodrigo Franco (Caffo) <caffeine@gmail.com>
2008-03-18 02:41:31 -0300

    Author is now required (validation)

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,9 +9,10 @@ class Book < ActiveRecord::Base
   
   has_one :cover
   
-  validates_presence_of :title,  :message => "can't be blank"
-  validates_presence_of :url,    :message => "can't be blank"
-  validates_presence_of :blurb,  :message => "can't be blank"
+  validates_presence_of :title,   :message => "can't be blank"
+  validates_presence_of :url,     :message => "can't be blank"
+  validates_presence_of :blurb,   :message => "can't be blank"
+  validates_presence_of :author,  :message => "can't be blank"
   
   acts_as_state_machine :initial => :next
   state :next
