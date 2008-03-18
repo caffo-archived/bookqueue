b97b247b8d3fcbb93c2bad6da050355c1bbfacec
Rodrigo Franco (Caffo) <caffeine@gmail.com>
2008-03-18 02:31:28 -0300

    Covers are now displayed using a helper.

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -19,4 +19,31 @@ def show_rating(stars)
   response << '</div>'
 end
 
+def show_cover(book,size)
+  case size
+  when "small"
+    height = "66px"
+    width  = "49px"
+  when "medium"
+    height = "98px"
+    width  = "74px"
+  when "large"
+    height = "164px"
+    width  = "123px"
+  when "original"
+    height = "246px"
+    width  = "184px"    
+  end
+  
+  if !book.cover.nil?
+		response = image_tag book.cover.public_filename, {:style => "height: #{height}; width: #{width}"}
+  elsif !book.image.empty?
+		response = image_tag book.image, {:style => "height: #{height}; width: #{width}"}		
+  else
+		response = image_tag '/images/default_cover.gif', {:style => "height: #{height}; width: #{width}"}
+  end
+  
+  return response
+end
+
 end
