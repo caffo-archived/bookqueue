267b90fa061c0e7e3da2c9bda266e6eeee9fc070
Rodrigo Franco (Caffo) <caffeine@gmail.com>
2008-03-21 22:38:11 -0300

    Added facebox configuration into main layout and css / js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+jQuery(document).ready(function($) {
+  $('a[rel*=facebox]').facebox() 
+})
