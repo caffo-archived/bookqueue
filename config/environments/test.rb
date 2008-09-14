b5b2be2173dd380a4b8fd0a4ff8939784c18290d
rodrigo franco (caffo) <caffeine@gmail.com>
2008-09-14 20:25:31 -0300

    Revert "project update to 2.1"
    This reverts commit 15e4d53520083be1ace7e1bc15355e0098feaae8.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,10 +13,7 @@ config.whiny_nils = true
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
 
-# Disable request forgery protection in test environment
-config.action_controller.allow_forgery_protection    = false
-
-# Tell Action Mailer not to deliver emails to the real world.
+# Tell ActionMailer not to deliver emails to the real world.
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
