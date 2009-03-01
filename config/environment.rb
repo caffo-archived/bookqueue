14e2b74fe02081adc2eec7a266f2bf4ebc9d8651
rodrigo franco (caffo) <caffeine@gmail.com>
2009-03-01 00:48:47 -0300

    environment updated

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails::Initializer.run do |config|
     File.directory?(lib = "#{dir}/lib") ? lib : dir
   end
 
-    config.time_zone = 'UTC'
+  config.time_zone = 'UTC'
   
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
@@ -16,13 +16,9 @@ Rails::Initializer.run do |config|
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
     :session_key => '_bookqueue_session',
-    :secret      => 'SECRET1111111111111111111111111111111'
+    :secret      => 'SECRET'
   }
 
-  # Use the database for sessions instead of the cookie-based default,
-  # which shouldn't be used to store highly confidential information
-  # (create the session table with "rake db:sessions:create")
-  # config.action_controller.session_store = :active_record_store
 end
 
 require 'configatron'
