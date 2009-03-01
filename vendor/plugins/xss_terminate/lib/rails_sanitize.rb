b9c46f1172f1faa028ac8446d63cf6c08fc3c7d1
rodrigo franco (caffo) <caffeine@gmail.com>
2009-03-01 00:30:04 -0300

    xss_terminate bug fixed

--- vendor/plugins/xss_terminate/lib/rails_sanitize.rb
+++ vendor/plugins/xss_terminate/lib/rails_sanitize.rb
@@ -1,4 +1,5 @@
 # This class exists so including the Rails HTML sanitization helpers doesn't polute your models.
 class RailsSanitize
   include ActionView::Helpers::SanitizeHelper
+  extend ActionView::Helpers::SanitizeHelper::ClassMethods
 end
