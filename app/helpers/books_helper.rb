aa7950331cffa75b39bf79bda8608bde0ece0070
Rodrigo Franco <caffeine@gmail.com>
2010-01-19 19:09:08 -0700

    Small fix in the 'Pages Read' graphic

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,43 +2,43 @@ module BooksHelper
   def seo_url(book)
     "#{book_url(book)}-#{book.title.to_slug}"
   end
-  
+
   def pages_graphic(report,div)
     data     = ""
     labels   = ""
-    colors   = ""
-    
-    report.each do |i|      
+    colors   = "968160,"
+
+    report.each do |i|
       data   << " [0, #{i[:pages]}]"
-      data   << "," unless i == report.last            
+      data   << "," unless i == report.last
 
       colors << "'968160'"
-      colors << "," unless i == report.last            
-      
+      colors << "," unless i == report.last
+
       labels << " '"
       labels << "#{i[:month].strftime('%b/%y')}"
       labels << "'"
       labels << "," unless i == report.last
     end
-    
+
     response = "<script type='text/javascript' charset='utf-8'>
     $(function () {
-      var api = new jGCharts.Api(); 
-      var opt = { 
+      var api = new jGCharts.Api();
+      var opt = {
       data : [#{data}],
       axis_labels : [#{labels}],
       colors : [#{colors}],
       bg   : 'E3D9BD',
-      bar_width   : 18, 
+      bar_width   : 18,
       size : '600x150',
       bar_spacing : 3,
-      bg_type     : 'stripes', 
-      bg_angle    : 90, 
-      bg_offset   : 'dfd2b1', 
+      bg_type     : 'stripes',
+      bg_angle    : 90,
+      bg_offset   : 'dfd2b1',
       bg_width    : 20,
       }
-      jQuery('<img>') 
-      .attr('src', api.make(opt)) 
+      jQuery('<img>')
+      .attr('src', api.make(opt))
       .appendTo('##{div}');
     });
     </script>"
